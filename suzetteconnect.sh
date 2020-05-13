e44e53d3e016e5084dbe23f8654536f193d2cfae
Maxime OLIVIE <maxime.olivie@etu.univ-lyon1.fr>
2020-05-13 15:12:20 +0200

    modif suzettescript

--- suzetteconnect.sh
+++ suzetteconnect.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "UCBL1 Login : "$1 
 echo "Warning : After entering the password Don't kill this process as long as you use --suzette-check"
-ssh -N -L 8080:192.168.77.13:3389 p1710179@linuxetu.univ-lyon1.fr
+ssh -N -L 8080:192.168.77.13:3389 $1@linuxetu.univ-lyon1.fr
