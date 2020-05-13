48c69c1555f6d848a4384685373771ded90c0d1b
OLIVIE MAXIME p1710179 <maxime.olivie@etu.univ-lyon1.fr>
2020-05-13 15:33:09 +0200

    Update suzetteconnect.sh

--- suzetteconnect.sh
+++ suzetteconnect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ${#1} -lt 8 ]; then
+  echo "Wrong username UCBL : pass a valid UCBL login ! [p1234567] form."
+  exit 2
+fi
+
 echo "UCBL1 Login : "$1 
 echo "Warning : After entering the password Don't kill this process as long as you use --suzette-check"
 ssh -N -L 8080:192.168.77.13:3389 $1@linuxetu.univ-lyon1.fr
